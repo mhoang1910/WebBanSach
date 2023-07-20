466b8bf81db0fbfcf6cbc524439e389289bc8567
hoangtnmph26276 <hoangtnmph26276@fpt.edu.vn>
2023-07-20 16:39:08 +0700

    .

---
+++ src/main/java/com/example/fpt/hoangtnm/HoangtnmApplication.java
@@ -0,0 +1,13 @@
+package com.example.fpt.hoangtnm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoangtnmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoangtnmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fpt/hoangtnm/HoangtnmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fpt.hoangtnm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoangtnmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
